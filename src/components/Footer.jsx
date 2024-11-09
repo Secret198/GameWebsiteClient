eabb5dd290e04efe5981674410b8e29646f6f5b1
Secret198 <coconutisthebest3@gmail.com>
2024-11-09 18:42:18 +0100

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+function Footer() {
+    return (
+        <div>
+            <p>Some stuff about the footer</p>
+            <p>Some stuff about the footer</p>
+            <p>Some stuff about the footer</p>
+            <p>Some stuff about the footer</p>
+            <p>Some stuff about the footer</p>
+            <p>Some stuff about the footer</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Posztok</a></li>
+                <li><a href="#">Felhasználók</a></li>
+                <li><a href="#">Felhasználói adatok</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
