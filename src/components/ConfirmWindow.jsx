ad015f5902c292a9a45ee8ce1c25aa7c481992f1
Roland <roli.rozsalyi@gmail.com>
2025-01-28 14:57:17 +0100

    what you did

--- src/components/ConfirmWindow.jsx
+++ src/components/ConfirmWindow.jsx
@@ -4,6 +4,7 @@ export default function ConfirmWindow({ text, functionToCall, setShow }) {
     return (
         <div className="centerScreen confirmWindow">
             <h1>{text}</h1>
+            <hr/>
             <button onClick={functionToCall}>Igen</button>
             <button onClick={() => setShow(false)}>Nem</button>
         </div>
--- src/components/FilterOptions.jsx
+++ src/components/FilterOptions.jsx
@@ -2,20 +2,26 @@
 
 export default function FilterOptions({changeSortBy, changeSortDir, search, mode}){
     return (
-        <>
-            <select name="sortBy" id="sortBy" onChange={changeSortBy} defaultValue={"id"}>
-                <option value="id">Relevencia idk??</option>
-                <option value="created_at">Dátum</option>
-                {mode == "post" && <option value="likes">Like</option>}
-            </select>
-            <select name="sortDir" id="sortDir" onChange={changeSortDir} defaultValue={"asc"}>
-                <option value="asc">Növekvő</option>
-                <option value="desc">Csökkenő</option>
-            </select>
-            <form onSubmit={search}>
-                <input type="text" name="searchBar" id="searchBar" />
-                <button type="submit">Keresés</button>
-            </form>
-        </>
+        
+            <div className="searchContainer">
+                <form className="searchForm" onSubmit={search}>
+                    <input type="text" name="searchBar" id="searchBar" />
+                    <button className="searchButton" type="submit">Keresés</button>
+                </form>
+        
+                <div className="sortContainer">
+                    <select name="sortBy" id="sortBy" onChange={changeSortBy} defaultValue={"id"}>
+                        <option value="id">Relevencia idk??</option>
+                        <option value="created_at">Dátum</option>
+                        {mode == "post" && <option value="likes">Like</option>}
+                    </select>
+                    <select name="sortDir" id="sortDir" onChange={changeSortDir} defaultValue={"asc"}>
+                        <option value="asc">Növekvő</option>
+                        <option value="desc">Csökkenő</option>
+                    </select>
+                </div>
+            </div>
+            
+        
     )
 }
